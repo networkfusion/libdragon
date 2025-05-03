a85b0bb6d32c39c0e1f2ebbcd034903159887373
networkfusion <networkfusion@users.noreply.github.com>
2025-05-03 12:59:35 +0000

    deploy: 11b362fe5005eaf5247b523c4382bdb59a3e0fba

--- ref/search/typedefs_d.js
+++ ref/search/typedefs_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['waveformread_0',['WaveformRead',['../mixer_8h.html#ab0e995bf04880a3e14f689f48fdb2475',1,'mixer.h']]]
+  ['waveformread_0',['WaveformRead',['../mixer_8h.html#ab0e995bf04880a3e14f689f48fdb2475',1,'mixer.h']]],
+  ['waveformstart_1',['WaveformStart',['../mixer_8h.html#ac0b60f65c1fdb178ad9e80df348a46e7',1,'mixer.h']]]
 ];
